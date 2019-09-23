989f8bafb362287a43d678f75d9170e5edfff1b1
Prabhu <prabhu.rk90@gmail.com>
2019-09-23 15:22:37 +0530

    Initial Test Release

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # The-Carousel-Project
-My first reusable CAROUSEL PROJECT
+
+A small Carousel library.
+
+## Installation
+
+    `npm install the-carousel-project`
+
+## Usage
+
+    const slideCarousel = require('the-carousel-project');
+
+## Tests
+
+    `npm test`
---
+++ test/test.js
@@ -0,0 +1,12 @@
+/* global describe,it */
+/* eslint-env mocha */
+'use strict';
+
+const { expect } = require('chai');
+const Carousel = require('../index');
+
+describe('Carousel', () => {
+  it('should create a carousel', () => {
+    expect(true).to.equal(true);
+  });
+});
